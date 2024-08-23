b86165bc7336c5b14190470c8daf7001044212bb
Nikhilesh <nikhileshg02@gmail.com>
2024-08-24 03:18:54 +0530

    get ai suggestions done

--- src/app/(app)/u/[username]/page.tsx
+++ src/app/(app)/u/[username]/page.tsx
@@ -1,7 +1,7 @@
 "use client"
  
 import { useParams } from 'next/navigation';
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import { z } from "zod";
 import { zodResolver } from '@hookform/resolvers/zod';
 import {useForm} from 'react-hook-form';
@@ -19,6 +19,11 @@ import {
 import { Input } from "@/components/ui/input"
 import { Textarea } from '@/components/ui/textarea';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import axios, { AxiosError } from 'axios';
+import { IApiResponse } from '@/types/IApiResponse';
+import { useToast } from '@/components/ui/use-toast';
+import { Skeleton } from "@/components/ui/skeleton"
+
 
 const formSchema = z.object({
   title: z.string().min(5).max(60),
@@ -28,6 +33,9 @@ const formSchema = z.object({
 const Page = () => {
   
   const {username}=useParams();
+  const {toast}=useToast();
+  const [messages,setMessages]=useState<string[]>([]);
+  const [isGettingSuggestions,setIsGettingSuggestions]=useState<boolean>(false);
 
   const form=useForm<z.infer <typeof formSchema>>({
     resolver:zodResolver(formSchema),
@@ -40,9 +48,41 @@ const Page = () => {
   const onSubmit=async(data:z.infer<typeof formSchema>)=>{
     console.log({...data,username});
   }
+
+  const suggestMessages=useCallback(async()=>{
+    setIsGettingSuggestions(true);
+    try {
+      const res=await axios.get<IApiResponse>('/api/suggest-messages');
+      console.log(res)
+      if(res.status===200 && res.data.success){
+        console.log(res.data.message);
+        const suggestions=res.data.message;
+        const temp= suggestions.split('||')
+        console.log(temp)
+        setMessages(temp);
+      } else{
+        toast({
+          title:"Error",
+          description:"Failed to generate message suggestions",
+          variant:"destructive"
+        })
+      }
+    } catch (error) {
+      console.error(error);
+      const axiosError=error as AxiosError<IApiResponse>;
+      toast({
+        title:"Error",
+        description:axiosError.response?.data.message || "Failed to generate message suggestions",
+        variant:"destructive"
+      })
+    } finally{
+      setIsGettingSuggestions(false);
+    }
+  },[]);
   
+
   return (
-    <div className=''>
+    <div className='dark'>
       <h1 className='text-3xl text-center font-medium mt-5 mb-3 '>Welcome to {username}&apos;s page</h1>
 
       {/* send anonymous msg */}
@@ -92,13 +132,22 @@ const Page = () => {
       <div className="w-5/6 md:w-3/4 lg:w-1/2 mx-auto mb-10">
         <Card className="w-full">
           <CardHeader>
-            <Button className='w-40'>Suggest messages</Button>
+            <Button onClick={suggestMessages} className='w-40'>Suggest messages</Button>
             <CardDescription>Get auto generated suggestions from AI and click to select</CardDescription>
           </CardHeader>
           <CardContent className='flex flex-col gap-2'>
-            <Button className='text-wrap' variant="outline">Cancel</Button>
-            <Button className='text-wrap' variant="outline">Cancel</Button>
-            <Button className='text-wrap' variant="outline">Cancel</Button>
+            {
+              messages.length!==0 ? messages?.map((message,index)=>(
+                <Button key={index} className='text-wrap' variant="outline">{message}</Button>
+              )) : 
+                isGettingSuggestions ? 
+                  <div className="flex flex-col gap-2">
+                    <Skeleton className="w-full h-[40px] rounded-xl" />
+                    <Skeleton className="w-full h-[40px] rounded-xl" />
+                    <Skeleton className="w-full h-[40px] rounded-xl" />
+                  </div>
+                  : <p>Click above button to generate messages</p>
+            }
           </CardContent>
         </Card>
       </div>
--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -1,22 +1,40 @@
-import { openai } from '@ai-sdk/openai';
-import { streamText } from 'ai';
+import axios from "axios";
 
-// Allow streaming responses up to 30 seconds
-export const maxDuration = 30;
-export const runtime = 'edge';
-
-
-export async function POST(req: Request) {
+export async function GET(req: Request) {
   try {
-    const prompt =
-        "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
-
-    const result = await streamText({
-      model: openai('gpt-3.5-turbo-instruct'),
-      prompt,
-      maxTokens:400
-    });
-    return Response.json({ success: true,suggestions: result.toDataStreamResponse() },{status:200});
+    const prompt = " Create a list of three creative,unique,open-ended and engaging questions formatted as a single string. Each question should be separated by '||'.Each question must be within 100 characters. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. Dont give any intro, outro to response. Dont add serial number or index or any number at all. Generatre questions different from example. Overall response must not exceed 300 characters . For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment' "
+    
+    const aiRes = await axios.post(`${process.env.AI_BASE_URL}`,{
+      "model": process.env.MODEL,
+      "messages": [
+        {
+          "role": "system",
+          "content": "You are a helpful and creative assistant, you get lot of questions"
+        },
+        {
+          "role": "user",
+          "content":prompt
+        }
+      ],
+      "max_tokens": 512,
+      "frequency_penalty": 0,
+      "logit_bias": {"2435":-100, "640":-100},
+      "logprobs": true,
+      "top_logprobs": 1,
+      "n": 1,
+      "presence_penalty": 0,
+      "response_format": { "type": "text" },
+      "stop": null,
+      "stream": false,
+      "temperature": 1.2,
+      "top_p": 1
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        "Authorization":`Bearer ${process.env.AI_API_KEY}`
+      }});
+    
+    return Response.json({ success: true,message: aiRes.data.choices[0].message.content },{status:200});
   } catch (error) {
     console.log("Some error occured",error);
     return Response.json({ success: false, message: "Failed to get suggestions" },{status:500});
