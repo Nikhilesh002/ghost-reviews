8e1b6ba25f96d5881a4c8ca21f62ca4c7339f3b6
Nikhilesh <nikhileshg02@gmail.com>
2024-08-25 03:31:43 +0530

    dynamic route fixed

--- src/app/api/check-username-unique/route.ts
+++ src/app/api/check-username-unique/route.ts
@@ -2,7 +2,7 @@ import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 import {z} from "zod";
 import {usenameValidation} from "@/schemas/signUpSchema";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 const UsernameQuerySchema=z.object({
   username:usenameValidation
@@ -13,12 +13,18 @@ export async function GET(req:NextRequest) {
   try {
     const searchParams = req.nextUrl.searchParams;
     const username=searchParams.get('username');
+    if (!username) {
+      return NextResponse.json(
+        { success: false, message: "Username query parameter is required" },
+        { status: 400 }
+      );
+    }
     // validate with zod
     const res=UsernameQuerySchema.safeParse({username});
     console.log("huhu",res)
     if(!res.success){
       const usernameErrors=res.error.format().username?._errors || [];
-      return Response.json({
+      return NextResponse.json({
         success:false,
         message:usernameErrors?.length>0
                 ? usernameErrors.join(',')
@@ -28,12 +34,12 @@ export async function GET(req:NextRequest) {
 
     const existingVerifiedUser=await UserModel.findOne({username,isVerified:true});
     if(existingVerifiedUser){
-      return Response.json({ success: false, message: "Username already taken" },{status:500});
+      return NextResponse.json({ success: false, message: "Username already taken" },{status:500});
     }
 
-    return Response.json({ success: true, message: "Username is unique" },{status:200});
+    return NextResponse.json({ success: true, message: "Username is unique" },{status:200});
   } catch (error) {
     console.log("Failed to check username",error);
-    return Response.json({ success: false, message: "Failed to check username" },{status:500});
+    return NextResponse.json({ success: false, message: "Failed to check username" },{status:500});
   }
 }
